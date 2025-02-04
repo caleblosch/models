9802d514a1878268df9c66cbb743e83d7c73c6d6
Caleb Losch <closch@ufl.edu>
2025-02-04 12:42:27 -0500

    Downloaded smaller model for better performance and quantization

--- research/object_detection/model_downloader.py
+++ research/object_detection/model_downloader.py
@@ -1,7 +1,7 @@
 import wget
-model_link = "http://download.tensorflow.org/models/object_detection/tf2/20200711/efficientdet_d0_coco17_tpu-32.tar.gz"
+model_link = "http://download.tensorflow.org/models/object_detection/tf2/20210210/centernet_mobilenetv2fpn_512x512_coco17_od.tar.gz"
 wget.download(model_link)
 import tarfile
-tar = tarfile.open('efficientdet_d0_coco17_tpu-32.tar.gz')
+tar = tarfile.open('centernet_mobilenetv2fpn_512x512_coco17_od.tar.gz')
 tar.extractall('.')
 tar.close()
