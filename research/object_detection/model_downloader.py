06f67a26a1de5ce4d38b9a8a6c675758d5b4814e
Caleb Losch <closch@ufl.edu>
2025-02-19 09:44:29 -0500

    Updated model being trained to it could be quantized

--- research/object_detection/model_downloader.py
+++ research/object_detection/model_downloader.py
@@ -1,7 +1,7 @@
 import wget
-model_link = "http://download.tensorflow.org/models/object_detection/tf2/20210210/centernet_mobilenetv2fpn_512x512_coco17_od.tar.gz"
+model_link = "http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_mobilenet_v2_320x320_coco17_tpu-8.tar.gz"
 wget.download(model_link)
 import tarfile
-tar = tarfile.open('centernet_mobilenetv2fpn_512x512_coco17_od.tar.gz')
+tar = tarfile.open('ssd_mobilenet_v2_320x320_coco17_tpu-8.tar.gz')
 tar.extractall('.')
 tar.close()
